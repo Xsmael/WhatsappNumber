89a3a84dee30aff57dae66c517389fea5212e186
Xsmael <puissancisma@gmail.com>
2020-11-24 20:57:22 +0000

    added xhr request

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ chrome.runtime.onInstalled.addListener(function () {
         color: '#3aa757'
     }, function () {
         console.log("The color is green.");
+        sendRequest();
     });
 });
 
@@ -17,3 +18,21 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
 });
+
+
+function sendRequest() {
+    console.log("Sending request");
+    console.log("Sending request");
+    var req = new XMLHttpRequest();
+    req.open("GET", "http://www.google.com/search?hl=en&q=ajax", true);
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                alert(req.responseText);
+                document.write("OK");
+                console.log(req.responseText);
+            }
+          }
+        };
+      req.send();
+} 
