1542f5e032f01fe94eed0e7db8ef3a12d076a4e3
Xsmael <puissancisma@gmail.com>
2020-11-30 10:38:23 +0000

    licensing

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
+/*TODO:
+- Remove phone numbers from title attribute
+*/
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({
         color: '#3aa757'
     }, function () {
         console.log("The color is green.");
-        sendRequest();
     });
 });
 
@@ -20,19 +22,40 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
 });
 
 
-function sendRequest() {
-    console.log("Sending request");
-    console.log("Sending request");
-    var req = new XMLHttpRequest();
-    req.open("GET", "http://www.google.com/search?hl=en&q=ajax", true);
-    req.onreadystatechange = function() {
-        if (req.readyState == 4) {
-            if (req.status == 200) {
-                alert(req.responseText);
-                document.write("OK");
-                console.log(req.responseText);
-            }
-          }
-        };
-      req.send();
-} 
+function generateUUID() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
+}
+
+chrome.storage.sync.get('UUID', function(items) { // UUID is the UUID
+    var userid = items.UUID;
+    if (userid) {
+        console.log("UUID= "+userid);
+    } else {
+        userid = generateUUID();
+        console.log("Generated UUID= "+userid);
+        chrome.storage.sync.set({UUID: userid});
+    }
+});
+
+chrome.storage.sync.get('license_key', function(items) { // whatever is the UUID
+    var license_key = items.license_key;
+    if (license_key && license_key.length ) {
+        console.log("License key= "+license_key);
+    } 
+    else {
+        console.log("NO License key found");
+        // chrome.storage.sync.set({license_key: license_key}, function() {
+        // });
+    }
+    function useToken(userid) {
+        // TODO: Use user id for authentication or whatever you want.
+    }
+});
--- popup.js
+++ popup.js
@@ -1,11 +1,34 @@
-let changeColor = document.getElementById('changeColor');
+let downloadBtn = document.getElementById('downloadBtn');
+let activateBtn = document.getElementById('activateBtn');
+let authPage = document.getElementById('auth');
+let unAuthPage = document.getElementById('un-auth');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+var UUID;
 
-changeColor.onclick = function(element) {
+function verifyAuth() {
+  chrome.storage.sync.get('license_key', function(items) { // whatever is the UUID
+    var license_key = items.license_key;
+    if (license_key && license_key.length )
+    {
+      console.log("verifyAuth SUCCESS");
+      authPage.style.display="visible";
+      unAuthPage.style.display="none";
+    } 
+    else 
+    {
+      console.log("verifyAuth FAIL");
+      authPage.style.display="none";
+      unAuthPage.style.display="visible";
+    }
+    function useToken(userid) {
+        // TODO: Use user id for authentication or whatever you want.
+    }
+  });
+}
+verifyAuth();
+
+downloadBtn.onclick = function(element) {
+  alert("downloadBtn");
   let filename = document.getElementById('groupName').value;
   let desc = document.getElementById('groupDesc').value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -19,5 +42,36 @@ changeColor.onclick = function(element) {
   });
 };
 
+activateBtn.onclick = function(element) {
+  let key = document.getElementById('key').value;
+  let phone = document.getElementById('phone').value;
+  sendRequest(key,phone);
+};
 
 
+chrome.storage.sync.get('UUID', function(items) { // UUID is the UUID
+  UUID = items.UUID;
+  if (!UUID) { console.error("UUID NOT FOUND"); } 
+});
+
+function sendRequest(key,phone) {
+  var req = new XMLHttpRequest();
+  var params= "q=auth&key="+key+"&phone="+phone+"&userUID="+UUID;
+  // req.open("POST", "http://160.226.184.146:8008/fetch", true);
+  req.open("POST", "http://localhost/WhatsappNumberServer/gateway.php", true);
+  req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  req.onreadystatechange = function() {
+      if (req.readyState == 4) {
+          if (req.status == 200) {
+              alert(req.responseText);
+              var res= JSON.parse(req.responseText);
+              if(res.success) {
+                chrome.storage.sync.set({license_key: res.license_key}, function() {
+                  verifyAuth();
+                });
+              }
+          }
+        }
+      };
+    req.send(params);
+} 
