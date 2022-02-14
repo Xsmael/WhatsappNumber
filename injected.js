dd257d40ede0cce1b1a8b315c1be5c71d8d5217b
Xsmael <puissancisma@gmail.com>
2022-02-14 20:31:54 +0100

    update

--- injected.js
+++ injected.js
@@ -1,7 +1,11 @@
-(function download() {    filename = document.querySelectorAll("#main span[title]")[0].getAttribute("title") || "contacts.txt";
+(function download() {    
+    groupName = document.querySelectorAll("#main span[title]")[0].getAttribute("title");
+    filename = "groupe "+groupName+".txt" || "contacts.txt"
     desc = desc || "No Description";
-    let header=  "Description: "+desc+"\r\n-----------------------\r\n \r\n";
-    let text=document.querySelectorAll("#main span[title]")[1].getAttribute("title");
+    let header=  "Groupe: "+groupName+"\r\n\r\n";
+        header+= "Description: "+desc+"\r\n\r\n";
+        header+= "Membres:\r\n=================\r\n \r\n";
+    let text=document.querySelectorAll("#main span[title]")[1].getAttribute("title").replace(/, /g, '\r\n');
     // let text=document.querySelector("span[title^='+']").getAttribute("title").replace(/,/g, '\r\n');
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(header+text));
